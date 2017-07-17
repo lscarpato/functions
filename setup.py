adb4933080f22b8351ce4dddc923cbe198be1dac
Luciana Gomes Scarpato <lgomessc@usersad.everis.int>
2017-07-17 09:17:50 -0300

    adding setup.py-5

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 # setup.py
 # Data Modificação: 17/07/2017
 # Consultor: Luciana Scarpato
-# Version 1.0.1
+# Version 0.1.6
 # testado com Python 2.7
 
 setup(name='functions',
-      version='1.0',
+      version='0.1.6',
       description='Python Distribution Utilities',
       author='Luciana Scarpato' ,
       author_email='luciana.scarpato@gmail.com',
