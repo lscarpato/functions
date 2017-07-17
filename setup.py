1a3bc79a207f64e4a32352c822282f2f7c0789c6
Luciana Gomes Scarpato <lgomessc@usersad.everis.int>
2017-07-17 08:55:48 -0300

    adding setup.py-3

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-# setup.py
-# Data Modificação: 17/07/2017
-# Consultor: Luciana Scarpato 
-# Version 1.0.1
-# testado com Python 2.7
-
-setup(name='functions',
-      version='1.0.1',
-      description='Python Distribution Utilities',
-      author='Luciana Scarpato ,
-      author_email='luciana.scarpato@gmail.com',
-      url='https://github.com/lscarpato/functions.git',
-     )
+setup(
+    name="functions",
+    version="1.0",
+)
