28ecd0fc6c528f52dcc8e93c6147dde9997d66e4
Janes <fabio.ljanes@gmail.com>
2017-07-24 09:53:05 -0300

    ajustes

--- functions/sum_function.py
+++ functions/sum_function.py
@@ -3,7 +3,7 @@ class SumFunction():
 
     def do_add_one(self, x):
        new_x = x+1
-       print 'x value: sal' + str(new_x)
+       print 'x value: sal teste4' + str(new_x)
        return new_x
        
         
